a9d5d2392a0ca4c17a53a5a6b5f898eaffac0350
Kirill <suslovsecondson@mail.ru>
2019-07-01 11:33:16 +0300

    Sortirovka spiska/ massiva

---
+++ ArraySort/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int[] mas = new int[] {1, 7, 2, 4, 3, 5};
+	Arrays.sort(mas);
+            System.out.printf(Arrays.toString(mas));
+        }
+    }
+
